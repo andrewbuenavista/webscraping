bd4bd4e2b7579bc4a3a3c2a8e1894378c738a6fc
Andrew Buenavista <andrewbuenavista1@gmail.com>
2022-04-04 14:59:27 -0500

    trading

--- webscraping-tradingview.py
+++ webscraping-tradingview.py
@@ -42,3 +42,24 @@ for company in companies[:5]:
 #Limit = find with limit of 1
 #keyword: allText = Obj.find(id="title",class="text")
 
+stock_table = soup.findAll("tbody")
+
+stock_table = stock_table[0]
+
+rows = stock_table.findAll('tr')
+
+for row in rows[:5]:
+    cols = row.findAll('td')
+    name = cols[0].find('span',attrs={'class':'tv-screener__description'})
+    name = name.text.strip()
+    price = float(cols[1].text)
+    change_text = cols[2].text
+    change_float = float(change_text.strip('%'))
+
+    s_price = round(price/(1+(change_float/100)),2)
+
+    print(f"Company name: {name}")
+    print(f"Current Price: {price}")
+    print(f"% change: {change_text}")
+    print(f"origal price {s_price}")
+    input()
